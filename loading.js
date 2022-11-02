ed39699962d1447e21c7648e2a7d8ceb482a1357
wellsjohn220 <60244641+wellsjohn220@users.noreply.github.com>
2022-11-02 18:24:48 +1100

    Add files via upload

---
+++ loading.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    var div_box = "<div id='load-screen'><div id='loading'></div></div>";
+    $("body").prepend(div_box);
+    $("#load-screen")
+      .delay(800)
+      .fadeOut(500, function () {
+        $(this).remove();
+      });
+    console.log('loading image has been done...');
+  });
